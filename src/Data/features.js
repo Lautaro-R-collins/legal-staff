a6b35c685c333db5afd9e50932df2b044820fa59
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-10-23 09:31:47 -0300

    add data

---
+++ src/Data/features.js
@@ -0,0 +1,15 @@
+// data/features.js
+export const features = [
+  {
+    title: 'Derecho Civil',
+    description: 'Asesoramiento en casos civiles y familiares con experiencia.',
+  },
+  {
+    title: 'Derecho Penal',
+    description: 'Defensa y representación legal en casos penales.',
+  },
+  {
+    title: 'Derecho Laboral',
+    description: 'Asesoramiento en conflictos laborales y contratos.',
+  },
+];
