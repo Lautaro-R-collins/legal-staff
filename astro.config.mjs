bdd61e2705eb94087fa58a3ffc7994b12029ce85
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-10-24 21:52:11 -0300

    server node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  output: 'static',
+  adapter: node({
+    mode: "standalone", 
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
-});
+})
